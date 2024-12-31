e99c71434a66290447e5bec99384599f613e60f5
minjae823582358235 <mk1321@ic.ac.uk>
2024-12-31 13:15:20 +0000

    Added Documentation, and some other stats

--- Property Analysis.py
+++ Property Analysis.py
@@ -10,17 +10,17 @@ stationDF.head()
 # %%
 class Property():
     def __init__(self, 
-                 name,
-                 loc, 
-                 value, 
-                 housecost, 
-                 baserent,
-                 h1,
-                 h2,
-                 h3,
-                 h4,
-                 hotel,
-                 colour):
+                 name:str,
+                 loc:int, 
+                 value:int, 
+                 housecost:int, 
+                 baserent:int,
+                 h1:int,
+                 h2:int,
+                 h3:int,
+                 h4:int,
+                 hotel:int,
+                 colour:str):
         self.name=name
         self.loc=loc
         self.value=value
@@ -28,25 +28,33 @@ class Property():
         self.baserent=baserent
         self.houserent=[h1,h2,h3,h4,hotel]
         self.colour=colour
+        self.mortgageValue=value*0.5
+        self.mortgageRepay=self.mortgageValue*1.1
     def __str__(self):
         return 'Property:',str(self.name)+',',str(self.colour)
 class Utility():
     def __init__(self,
-                 name,
-                 loc,
-                 value):
+                 name:str,
+                 loc:int,
+                 value:int):
         self.name=name
         self.loc=loc
         self.value=value
+        self.mortgageValue=value*0.5
+        self.mortgageRepay=self.mortgageValue*1.1
     def __str__(self):
         return  'Utility:',str(self.name)
 class Station():
     def __init__(self,
-                 name,
-                 loc,
-                 value):
+                 name:str,
+                 loc:int,
+                 value:int):
         self.name=name
         self.loc=loc
         self.value=value
+        self.mortgageValue=value*0.5
+        self.mortgageRepay=self.mortgageValue*1.1
     def __str__(self):
         return  'Station:',str(self.name)
+    
+# %%
