9213d68f05abb838815810219f348a46f1efba54
minjae823582358235 <mk1321@ic.ac.uk>
2025-01-04 16:10:19 +0000

    Update Markov Analysis.py

--- Markov Analysis.py
+++ Markov Analysis.py
@@ -5,6 +5,11 @@ import numpy as np
 print(board[0])
 # %%
 print(type(board[0]))
+def diceroll(number:int):
+    if number <1 or number>12:
+        raise ValueError('number should be between 1 and 12')
+    dicerollArr=[1/36,2/36,3/36,4/36,5/36,6/36,5/36,4/36,3/36,2/36,1/36]
+    return dicerollArr[number-1]
 # %%
 class MarkovNode:
     def __init__(self,
@@ -14,6 +19,6 @@ class MarkovNode:
         self.innerds=innerds
         self.tag=tag
         self.outward=outward
-    def MarkovMatrix(markovNodeArray):
-        output=np.zeros(len(markovNodeArray),len(markovNodeArray))
-        
+def MarkovMonopolyMatrix(markovNodeArray):
+    output=np.zeros(len(markovNodeArray),len(markovNodeArray))
+    
