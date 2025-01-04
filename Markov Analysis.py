b286d20f9f450c89c251bc3a8688281778ab53be
minjae823582358235 <mk1321@ic.ac.uk>
2025-01-04 13:40:22 +0000

    Update Markov Analysis.py

--- Markov Analysis.py
+++ Markov Analysis.py
@@ -6,3 +6,14 @@ print(board[0])
 # %%
 print(type(board[0]))
 # %%
+class MarkovNode:
+    def __init__(self,
+                 innerds=None,
+                 tag=None,
+                 outward=None):
+        self.innerds=innerds
+        self.tag=tag
+        self.outward=outward
+    def MarkovMatrix(markovNodeArray):
+        output=np.zeros(len(markovNodeArray),len(markovNodeArray))
+        
