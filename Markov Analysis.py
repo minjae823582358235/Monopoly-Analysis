1687120fe7090be357a3fc78409ea90f4bc87276
minjae823582358235 <mk1321@ic.ac.uk>
2025-01-29 21:29:57 +0000

    Update Markov Analysis.py

--- Markov Analysis.py
+++ Markov Analysis.py
@@ -6,10 +6,10 @@ import numpy as np
 print(board[0])
 # %%
 def diceroll(number:int):
-    if number <1 or number>12:
-        raise ValueError('number should be between 1 and 12')
+    if number <2 or number>12:
+        raise ValueError('number should be between 2 and 12')
     dicerollArr=[1/36,2/36,3/36,4/36,5/36,6/36,5/36,4/36,3/36,2/36,1/36]
-    return dicerollArr[number-1]
+    return dicerollArr[number-2]
 # %%
 class MarkovNode:
     def __init__(self,
@@ -19,7 +19,7 @@ class MarkovNode:
         self.innerds=innerds
         self.tag=tag
         self.outward=outward
-def MarkovMonopolyMatrix(markovNodeArray):
+def MarkovMonopolyMatrix(markovNodeArray): #assuming markovnode array is sorted with ascending location
     output=np.zeros(len(markovNodeArray),len(markovNodeArray))
     counter=0
     for node in markovNodeArray:
@@ -36,8 +36,43 @@ def MarkovMonopolyMatrix(markovNodeArray):
             inputlist[index]=inputsdict[index]
         output[counter][minindex:maxindex+1]=inputlist
         counter+=1
-    print(output.shape)
+    print('matrix created')
+    return output
+def MarkovMatrixSolve(InitialState,Matrix):
+    if len(InitialState)!=len(Matrix):
+        raise ValueError('Initial state and matrix should have the same length')
+    if not np.allclose(np.sum(Matrix,axis=1),np.ones(len(Matrix))):
+        raise ValueError('Matrix should be a stochastic matrix')
+    output=InitialState
+    for i in range(100):
+        output=np.dot(output,Matrix)
+    return output
 
+def Board2MarkovArray(boardArray):
+    output=[]
+    for thing in boardArray:
+        probabilitydict={}
+        if thing.__class__.__name__=='Property' or thing.__class__.__name__=='Utility' or thing.__class__.__name__=='Station' or thing.__class__.__name__=='JailVisitng' or thing.__class__.__name__=='FreeParking' or thing.__class__.__name__=='Tax' or thing.__class__.__name__=='Go':
+            for move in range(2,13):
+                if move+thing.loc>40:
+                    probabilitydict[move+thing.loc-40]=diceroll(move)
+                if move+thing.loc==30:
+                    probabilitydict[10.5]=diceroll(move)
+                else:
+                    probabilitydict[move+thing.loc]=diceroll(move)
+            output.append(MarkovNode(innerds=thing,outward=probabilitydict,tag=thing.loc))
+        elif thing.__class__.__name__=='Chance':  
+        elif thing.__class__.__name__=='CommunityChest':
+        elif thing.__class__.__name__=='GoToJail':
+            probabilitydict[10.5]=1
+        elif thing.__class__.__name__=='InJail':
+            doubleP=diceroll(2)
+            doubleArr=[2,4,6,8,10,12]
+            for move in doubleArr:
+                probabilitydict[move+10]=doubleP
+            probabilitydict[10.5]=1-doubleP*len(doubleArr)
+        else:
+            raise ValueError('Class not recognized')
 #probabilities to consider:
 # chance
 # community chest
